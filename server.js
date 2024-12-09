a3fb647468784ac89760e5a1028035f53e508f4a
adam <adam.ksiazek.pmpg@gmail.com>
2024-12-09 10:40:59 +0100

    server - próbuję na hapi

---
+++ server.js
@@ -0,0 +1,28 @@
+require('dotenv').config();
+
+const Hapi = require('@hapi/hapi');
+
+const init = async () => {
+    const server = Hapi.server({
+        port: process.env.CONVERTER_SERVICE_HOST_PORT || 3005,
+        host: process.env.CONVERTER_SERVICE_HOST || 'localhost'
+    });
+
+    // Importowanie tras
+    const videoRoutes = require('./routes/video');
+    const aboutRoutes = require('./routes/about');
+
+    // Rejestrowanie tras
+    server.route(videoRoutes);
+    server.route(aboutRoutes);
+
+    await server.start();
+    console.log('Server running on %s', server.info.uri);
+};
+
+process.on('unhandledRejection', (err) => {
+    console.log(err);
+    process.exit(1);
+});
+
+init();
