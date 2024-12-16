006ca4322cae1e1a21b60d6de1e2cce849bae00c
aksiazek <adam.ksiazek.pmpg@gmail.com>
2024-12-16 12:43:39 +0100

    thumbnailRoutes.js - praca nad metodą

--- routes/thumbnailRoutes.js
+++ routes/thumbnailRoutes.js
@@ -1,20 +1,51 @@
 const express = require('express');
 const path = require('path');
+const fs = require('fs');
+
 const { generateThumbnails } = require('../services/thumbnailService');
 const { getVideoDuration } = require('../services/ffmpegUtils');
 
 const router = express.Router();
 
-router.post('/generate-thumbnails', async (req, res) => {
+router.post('/generate-thumbnails/:dirname', async (req, res) => {
     try {
-        const {inputFile, outputDir} = req.body;
-        if (!inputFile || !outputDir) {
-            return res.status(400).json({error: 'inputFile i outputDir są wymagane.'});
+        const { dirname } = req.params; // Pobieranie nazwy folderu z URL
+        const { frame, second } = req.body;
+        const setup = {
+            frame,
+            second
+        };
+        const inputFile = path.join(__dirname, '../output', dirname, `${dirname}.mp4`);
+        const outputDir = path.join(__dirname, '../output-hls', dirname );
+
+        if (!frame && !second) {
+            return res.status(400).json({error: 'Nie podałeś, którą stopklatkę!'});
         }
+        if (!fs.existsSync(inputFile)) {
+            return res.status(404).json({ error: 'Brak folderu wejściowego #102' });
+        }
+        if (!fs.existsSync(outputDir)) {
+            fs.mkdirSync(outputDir, { recursive: true });
+        }
+
+        // Oblicz czas trwania wideo
+        const duration = await getVideoDuration(inputFile);
+        setup.duration = duration;
+
+        await generateThumbnails(inputFile, outputDir, setup);
+
+        return res.status(200).json({
+            message: 'Stopklatki zostały wygenerowane',
+            data: {
+                second,
+                frame,
+            }
+
+        });
     } catch (error) {
         console.error('Błąd generowania stopklatek:', error);
         return res.status(500).json({
-            error: 'Nie udało się wygenerować stopklatek.',
+            error: 'Nie udało się wygenerować stopklatki!',
             details: error.message
         });
     }
