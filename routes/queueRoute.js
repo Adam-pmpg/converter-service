89a1a9faab76f3ca7c36648b865b1502bc9d2315
aksiazek <adam.ksiazek.pmpg@gmail.com>
2024-12-30 13:26:43 +0100

    refactor

--- routes/queueRoute.js
+++ routes/queueRoute.js
@@ -3,28 +3,24 @@ const { sendToQueue, QUEUE_NAME } = require('../services/rabbit/rabbitmq');
 
 const router = express.Router();
 router.post('/send-to-queue/', (req, res) => {
-    const { filePath, responseFromChunkUploader, folderId, hls_time } = req.body;
-
-    if (!filePath) {
-        return res.status(400).send('filePath is required!');
+    const { responseFromChunkUploader } = req.body;
+    const { outputFile } = responseFromChunkUploader;
+    if (!outputFile ) {
+        return res.status(400).send('Brak ścieżki do scalonego pliku oryginału!');
     }
 
-    run(res, filePath, responseFromChunkUploader);
+    run(res, responseFromChunkUploader);
 });
 
-async function run(res, filePath, data) {
+async function run(res, data) {
     try {
         // Wyślij wiadomość do RabbitMQ
         await sendToQueue(QUEUE_NAME, { data });
-        console.log({
-            a12: '*******',
-            filePath,
-            data,
-        })
-        res.status(200).json({message:`Wiadomość wysłana na kolejkę\noryginalne wideo: ${filePath}`});
+        let outputFile = data && data.outputFile ? data.outputFile : '';
+        res.status(200).json({message:`Wiadomość wysłana na kolejkę\noryginalne wideo: ${outputFile}`});
     } catch (error) {
-        console.error('Error sending message to queue:', error);
-        res.status(500).json({message:'Failed to send message to queue'});
+        console.error('Błąd wysyłania wiadomości do kolejki:', error);
+        res.status(500).json({message:'Błąd wysyłania wiadomości do kolejki!'});
     }
 }
 
